81742898b713c2c0c8b57dbb3dd04772ee27b5f2
Daniel Genaro <danielgenaro198@gmail.com>
2025-02-08 19:13:33 -0300

    feat(events): remove comments

--- internal/events/spy.go
+++ internal/events/spy.go
@@ -30,21 +30,3 @@ func (e EventSpy) GetPayload() any {
 func (eh EventHandlerSpy) Handle(event EventInterface, wg *sync.WaitGroup) {
 	defer wg.Done()
 }
-
-// type EventInterface interface {
-// 	GetName() string
-// 	GetDateTime() time.Time
-// 	GetPayload() any
-// }
-
-// type EventHandlerInterface interface {
-// 	Handle(event EventInterface, wg *sync.WaitGroup)
-// }
-
-// type EventDispatcherInterface interface {
-// 	Register(ctx context.Context, event string, handler EventHandlerInterface) error
-// 	Dispatch(ctx context.Context, event EventInterface) error
-// 	Remove(ctx context.Context, event string, handler EventHandlerInterface) error
-// 	Has(ctx context.Context, event string, handler EventHandlerInterface) bool
-// 	Clear(ctx context.Context) error
-// }
